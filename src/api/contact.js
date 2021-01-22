f32cba65b572a3d94b3c27f1349ae7c81b831015
Leon <toxiapo@gmail.com>
2021-01-22 15:44:49 -0500

    update api url http->https

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = 'http://www.json-generator.com/api/json/get/bUAgWyuTsi';
+const API = 'https://www.json-generator.com/api/json/get/bUAgWyuTsi';
 
 export function getAllContacts() { 
   return axios.get(API);
